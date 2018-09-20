106f579d8ded898dcf9df4a178dbde991dfa6d34
DPigeon <D_pig@encs.concordia.ca>
2018-09-20 16:02:19 -0400

    Seat Array
    Added an array for seats so that I can begin to enhance the seat window.

--- seats.cpp
+++ seats.cpp
@@ -1,11 +1,18 @@
 #include "seats.h"
 #include "ui_seats.h"
 
+using namespace std;
+
+string seatsArray[7][20]; //Array of all the seats
+
 Seats::Seats(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::Seats)
 {
     ui->setupUi(this);
+    for(int i = 0; i < 7; i++)
+        for(int j = 0; j < 20; j++)
+            seatsArray[i][j] == ""; //Initializing the seats to 'not taken'
 }
 
 Seats::~Seats()
