a2842a6b4ae61bf8faa41c0b2dc3cb8b53de8452
David <D_Pig@encs.concordia.ca>
2018-10-09 22:57:21 -0400

    Payment Page
    Enhanced the inputs of the page and added some getters

--- payment.cpp
+++ payment.cpp
@@ -9,6 +9,58 @@ Payment::Payment(QWidget *parent, QString p) :
     p = price;
 }
 
+/*All getters*/
+
+string Payment::getfName()
+{
+    return fname;
+}
+
+string Payment::getlName()
+{
+    return lname;
+}
+
+string Payment::getShipAddress()
+{
+    return shipAddress;
+}
+
+string Payment::getEmail()
+{
+    return email;
+}
+
+string Payment::getCardCode()
+{
+    return cardCode;
+}
+
+string Payment::getCardName()
+{
+    return cardName;
+}
+
+int Payment::getVerification()
+{
+    return verification;
+}
+
+int Payment::getExp1()
+{
+    return exp1;
+}
+
+int Payment::getExp2()
+{
+    return exp2;
+}
+
+int Payment::getExp3()
+{
+    return exp3;
+}
+
 Payment::~Payment()
 {
     delete ui;
--- payment.h
+++ payment.h
@@ -2,6 +2,9 @@
 #define PAYMENT_H
 
 #include <QDialog>
+#include <string>
+
+using namespace std;
 
 namespace Ui {
 class Payment;
@@ -13,11 +16,29 @@ class Payment : public QDialog
 
 public:
     explicit Payment(QWidget *parent, QString price);
+    string getfName();
+    string getlName();
+    string getShipAddress();
+    string getEmail();
+    string getCardCode();
+    string getCardName();
+    int getVerification();
+    int getExp1();
+    int getExp2();
+    int getExp3();
     ~Payment();
 
 private:
     QString price;
     Ui::Payment *ui;
+    string fname;
+    string lname;
+    string shipAddress;
+    string email;
+    string cardCode;
+    string cardName;
+    int verification;
+    int exp1, exp2, exp3; //The expiration card
 };
 
 #endif // PAYMENT_H
